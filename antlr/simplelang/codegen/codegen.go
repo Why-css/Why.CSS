c8f6140a15f0d1b99920f1525c19194576d7022d
chirbard <matamm@outlook.com>
2025-05-08 19:46:53 +0300

    feat: generate go from simple lang

---
+++ antlr/simplelang/codegen/codegen.go
@@ -0,0 +1,44 @@
+package codegen
+
+import (
+	"fmt"
+	"simplelang/ast"
+	"strings"
+)
+
+// GenerateGoCode takes a list of Stat nodes and returns Go code as a string.
+func GenerateGoCode(stats []ast.Stat) string {
+	var b strings.Builder
+	b.WriteString("package main\n\nimport \"fmt\"\n\nfunc main() {\n")
+	for _, stat := range stats {
+		b.WriteString("    ")
+		b.WriteString(genStat(stat))
+		b.WriteString("\n")
+	}
+	b.WriteString("}\n")
+	return b.String()
+}
+
+func genStat(stat ast.Stat) string {
+	switch s := stat.(type) {
+	case ast.Assign:
+		return fmt.Sprintf("%s := %s", s.Name, genExpr(s.Expr))
+	case ast.Print:
+		return fmt.Sprintf("fmt.Println(%s)", genExpr(s.Expr))
+	default:
+		return "// unknown statement"
+	}
+}
+
+func genExpr(expr ast.Expr) string {
+	switch e := expr.(type) {
+	case ast.IntLiteral:
+		return fmt.Sprintf("%d", e.Value)
+	case ast.VarRef:
+		return e.Name
+	case ast.BinOp:
+		return fmt.Sprintf("(%s %s %s)", genExpr(e.Left), e.Op, genExpr(e.Right))
+	default:
+		return "/* unknown expr */"
+	}
+}
--- antlr/simplelang/main.go
+++ antlr/simplelang/main.go
@@ -5,18 +5,39 @@ import (
 	"os"
 
 	"simplelang/ast"
+	"simplelang/codegen"
 	"simplelang/parser"
 
 	"github.com/antlr4-go/antlr/v4"
 )
 
 func main() {
-	input, _ := antlr.NewFileStream(os.Args[1])
+	// input, _ := antlr.NewFileStream(os.Args[1])
+	input, _ := antlr.NewFileStream("input")
 	lexer := parser.NewSimpleLangLexer(input)
 	stream := antlr.NewCommonTokenStream(lexer, 0)
 	p := parser.NewSimpleLangParser(stream)
 	tree := p.Prog()
 
-	ast := tree.Accept(&ast.ASTBuilder{})
-	fmt.Printf("%#v\n", ast)
+	builtAst := tree.Accept(&ast.ASTBuilder{})
+	fmt.Printf("%#v\n", builtAst)
+
+	// Generate Go code
+	goCode := codegen.GenerateGoCode(builtAst.([]ast.Stat))
+	err := os.MkdirAll("./generated", os.ModePerm)
+	if err != nil {
+		fmt.Println("Error creating directory:", err)
+		return
+	}
+	_, err = os.Create("./generated/generated.go")
+	if err != nil {
+		fmt.Println("Error creating output file:", err)
+		return
+	}
+	err = os.WriteFile("generated/generated.go", []byte(goCode), 0644)
+	if err != nil {
+		fmt.Println("Error writing Go code:", err)
+		return
+	}
+	fmt.Println("Go code written to generated.go")
 }
